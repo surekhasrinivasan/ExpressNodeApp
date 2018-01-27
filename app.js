63f3269f40667fc45c9fcca4bfa8d0930ca8a2da
surekha <surekhasrao@gmail.com>
2018-01-27 17:16:31 +0000

    Express Level 4 Assignment

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
-//Express Level 3 Assignment
+//Express Level 4 Assignment
 var express = require("express");
 var app = express();
+var bodyParser = require('body-parser');
+var parseUrlencoded = bodyParser.urlencoded({
+	extended: false
+});
 var cities = {
 	'Boston': 'Massachusetts',
 	'Princeton': 'New Jersey',
@@ -27,10 +31,19 @@ app.get('/cities/:name', function(request, response) {
 	var cityName = name[0].toUpperCase() + name.slice(1).toLowerCase();
 	var stateName = cities[cityName];
 	if (!stateName) {
-		response.status(404).json("State not found for " + cityName);
+		response.status(404).json("State not found");
 	} else {
-		response.json(cityName + " city is in the state of " + stateName);
+		response.json(stateName);
 	}
+});
+app.post('/cities', parseUrlencoded, function(request, response) {
+	var newCity = request.body;
+	cities[newCity.name] = newCity.stateName;
+	response.status(201).json(newCity.name);
+});
+app.delete('/cities/:name', function(request, response) {
+	delete cities[request.cityName];
+	response.sendStatus(200);
 });
 app.listen(process.env.PORT, function() {
 	console.log("Running Express..");
--- public/client.js
+++ public/client.js
@@ -3,11 +3,40 @@ $(function() {
 	$.get('/cities', appendToList);
 
 	function appendToList(cities) {
-		for (var i = 0; i < cities.length; i++) {
-			var list = document.createElement("OPTION");
-			list.setAttribute("value", cities[i]);
-			list.innerHTML = cities[i];
-			document.getElementById('selection').appendChild(list);
+		var list = [];
+		var content, city;
+		for (var i in cities) {
+			city = cities[i];
+			content = '<a href="#" data-block="' + city + '"><img src="deleteButton.png"</a>' + '  ' + '<a href = "/cities/' + city + '">' + city + '</a>';
+			list.push($('<li>', {
+				html: content
+			}));
 		}
+		$('.city-list').append(list);
 	}
+	$('form').on('submit', function(event) {
+		event.preventDefault();
+		var form = $(this);
+		var newData = form.serialize();
+		$.ajax({
+			type: 'POST',
+			url: '/cities',
+			data: newData
+		}).done(function(cityName) {
+			appendToList([cityName]);
+			form.trigger('reset');
+		});
+	});
+	$('.city-list').on('click', 'a[data-block]', function(event) {
+		if (!confirm('Are you sure?')) {
+			return false;
+		}
+		var target = $(event.currentTarget);
+		$.ajax({
+			type: 'DELETE',
+			url: '/cities/' + target.data('cityName')
+		}).done(function() {
+			target.parents('li').remove();
+		});
+	});
 });
